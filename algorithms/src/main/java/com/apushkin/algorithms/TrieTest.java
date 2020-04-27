0fe3d6f1f6022a3c5e6032c2b871bde07edd1699
apushkin <apushkin@ipmks.com>
2020-04-27 17:33:46 +0300

    Add multi-map naive realization

--- algorithms/src/main/java/com/apushkin/algorithms/TrieTest.java
+++ algorithms/src/main/java/com/apushkin/algorithms/TrieTest.java
@@ -1,5 +1,23 @@
 package com.apushkin.algorithms;
 
+/*
+* Есть строка, состоящая из слов. Все слова в ней разделены одним пробелом. Нужно преобразовать строку в такую
+* структуру данных, которая группирует слова по первой букве в слове. Затем вывести только группы, содержащие более
+* одного элемента.
+
+Группы должны быть отсортированы в алфавитном порядке. Слова внутри группы нужно сортировать по убыванию количества
+* символов; если количество символов равное, то сортировать в алфавитном порядке.
+
+Пример строки: String s = «сапог сарай арбуз болт бокс биржа»
+
+Отсортированная строка: [б=[биржа, бокс, болт], c=[caпог, сарай]]
+
+Ответ присылайте ссылкой на опубликованный приватный Gist. Комментарии и тесты приветствуются.
+*
+* */
+
+import java.util.*;
+
 public class TrieTest {
     public static void main(String[] args) {
         String input = "сапог сарай арбуз болт бокс биржа";
@@ -10,6 +28,30 @@ public class TrieTest {
             trie.insert(word);
         }
 
-        System.out.println("Done");
+        Map<Character, Set<String>> groups = new TreeMap<>();
+        for (String word : words) {
+            Character startingChar = word.charAt(0);
+            Set<String> set = groups.computeIfAbsent(startingChar, (strValue) -> new TreeSet<>());
+            set.add(word);
+            groups.putIfAbsent(word.charAt(0), set);
+        }
+
+        StringBuilder sb = new StringBuilder("[");
+        for (Iterator<Map.Entry<Character, Set<String>>> iterator = groups.entrySet().iterator(); iterator.hasNext(); ) {
+            Map.Entry<Character, Set<String>> entry = iterator.next();
+            Set<String> strings = entry.getValue();
+            if (strings.size() <= 1) {
+                continue;
+            }
+            sb.append(entry.getKey()).append("=[");
+            strings.forEach(str -> sb.append(str).append(", "));
+            sb.delete(sb.length() - 2, sb.length()).append("]"); //убираем ", " в конце группы перед закрывающей скобкой
+            if (iterator.hasNext()) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+
+        System.out.println(sb.toString());
     }
 }
